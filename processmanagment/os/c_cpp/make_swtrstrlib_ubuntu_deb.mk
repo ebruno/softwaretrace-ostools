b4c2a465484f7780ed4eec6ff6aa82986480ac2b
Eric Bruno <eric@ebruno.org>
2018-04-04 16:01:53 -0700

    initial changes for deb package generation on ubuntu

--- processmanagment/os/c_cpp/make_swtrstrlib_ubuntu_deb.mk
+++ processmanagment/os/c_cpp/make_swtrstrlib_ubuntu_deb.mk
@@ -12,10 +12,12 @@ SWTRSTRLIB_PRIVATE_BUILD_ID     = 0
 SWTRSTRLIB_BUILD_NUMBER     = 2
 
 SWTRSTRLIB_RELEASE_KEY     = 0
-
-SWTRSTRLIB_TARFILE_NAME   = ${TARFILE_NAME}-${SWTRSTRLIB_MAJOR}.${SWTRSTRLIB_MINOR}.${SWTRSTRLIB_SUBVERSION}.${SWTRSTRLIB_PRIVATE_BUILD_ID}.${SWTRSTRLIB_BUILD_NUMBER}.tar.gz
+#swtrstrlib_0.9.0.0.2.orig.tar.gz
+SWTRSTRLIB_TARFILE_NAME   = ${TARFILE_NAME}_${SWTRSTRLIB_MAJOR}.${SWTRSTRLIB_MINOR}.${SWTRSTRLIB_SUBVERSION}.${SWTRSTRLIB_PRIVATE_BUILD_ID}.${SWTRSTRLIB_BUILD_NUMBER}_orig.tar.gz
 
 SWTRSTRLIB_SRCDIR_NAME   = ${SWTRSTRLIB_NAME}-${SWTRSTRLIB_MAJOR}.${SWTRSTRLIB_MINOR}.${SWTRSTRLIB_SUBVERSION}.${SWTRSTRLIB_PRIVATE_BUILD_ID}.${SWTRSTRLIB_BUILD_NUMBER}
+
+
 
 SWTRCOMMON_FILES          = CMakeLists.txt Makefile_packages Makefile_doc
 SWTRSTRLIB_DOC_FILES 	  = 
@@ -26,6 +28,7 @@ swtrstrlib_all:
 		@echo "dummy_all"
 		@echo "DEBBUILD_BASE=${DEBBUILD_BASE}"
 		@echo "DEBBUILD_SOURCE_DIR=${DEBBUILD_SOURCE_DIR}"
+		@exit 0;
 
 swtrstrlib_create_src:
 	@echo "Creating Source Distribution ${SWTRSTRLIB_TARFILE_NAME}";
@@ -38,20 +41,5 @@ swtrstrlib_create_src:
 	@tar -czf ${DEBBUILD_SOURCE_DIR}/${SWTRSTRLIB_TARFILE_NAME} "${SWTRSTRLIB_SRCDIR_NAME}"
 	@rm -r -f "${SWTRSTRLIB_SRCDIR_NAME}";
 
-swtrstrlib_create_rpm:
-	@if ! test -d ${CURDIR}/${SPEC_DIR}; then \
-		mkdir -p ${CURDIR}/${SPEC_DIR}; \
-	fi;
-	@if test -d ${CURDIR}/${SPEC_TEMPLATE_DIR}; then \
-		cat ${SPEC_TEMPLATE_DIR}/${SWTRSTRLIB_NAME}.spec.tmpl | sed -e "s/XMAJOR/${SWTRSTRLIB_MAJOR}/g" > ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec; \
-		sed -e "s/XMINOR/${SWTRSTRLIB_MINOR}/g" -i ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec;\
-		sed -e "s/XSUBVERSION/${SWTRSTRLIB_SUBVERSION}/g" -i ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec;\
-		sed -e "s/XPRIVATE_BUILD_ID/${SWTRSTRLIB_PRIVATE_BUILD_ID}/g" -i ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec;\
-		sed -e "s/XBUILD_NUMBER/${SWTRSTRLIB_BUILD_NUMBER}/g" -i ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec;\
-		sed -e "s/XRELEASE_KEY/${SWTRSTRLIB_RELEASE_KEY}/g" -i ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec;\
-		rpmbuild $(DEB_VERBOSE} -bb ${SPEC_DIR}/${SWTRSTRLIB_NAME}.spec; \
-	else \
-	  echo "Error unable to locate ${CURDIR}/${SPEC_TEMPLATE_DIR}, fatal,terminating"; \
-	  exit 1; \
-	fi;
-
+swtrstrlib_create_pkg:
+		@echo "Currently Not implemented"
