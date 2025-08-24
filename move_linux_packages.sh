3d4203b104a7c10a84b066807f85856641bfccaf
Eric Bruno <eric@ebruno.org>
2025-08-24 13:13:08 -0700

    adjust script

--- move_linux_packages.sh
+++ move_linux_packages.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+env;
+echo "ID=${ID}";
 if [ "${ID}" = "arch" ]; then
     echo find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
 #find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Copying Package:%f\n" -exec basename {}; install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
