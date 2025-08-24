dad1c75df38cad5b40b7d60148dfbe844676ba1c
Eric Bruno <eric@ebruno.org>
2025-08-24 13:32:34 -0700

    set env vars

--- move_linux_packages.sh
+++ move_linux_packages.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/bash
+ARTIFACTDIR="$1"
+PACKAGE_EXT="$2"
+ID="$3"
+NAS_REPO_ROOT_PATH="$4"
+REPO_ROOT_NAME="$5"
+machine="$6"
 env;
-echo "ID=${ID}";
 if [ "${ID}" = "arch" ]; then
     echo find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
 #find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Copying Package:%f\n" -exec basename {}; install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
