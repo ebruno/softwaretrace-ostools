383567d60ec21ec113137d512164753b0586c764
Eric Bruno <eric@ebruno.org>
2025-08-24 18:05:17 -0700

    set env vars

--- move_linux_packages.sh
+++ move_linux_packages.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/bash
-ARTIFACTDIR="$1"
-PACKAGE_EXT="$2"
-ID="$3"
-NAS_REPO_ROOT_PATH="$4"
-REPO_ROOT_NAME="$5"
-machine="$6"
+ARTIFACTDIR="$1";
+PACKAGE_EXT="$2";
+ID="$3";
+NAS_REPO_ROOT_PATH="$4";
+REPO_ROOT_PATH="$5";
+REPO_NAME="$6";
+machine="$7";
 
 if [ "${ID}" = "arch" ]; then
-    echo find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
-    find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
-#find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Copying Package:%f\n" -exec basename {}; install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/sfwtrace/os/${machine} \;
-    cd ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux;
+    echo find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/${REPO_NAME}/os/${machine} \;
+    find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/${REPO_NAME}/os/${machine} \;
+    cd ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_NAME}/${ID}linux/${REPO_NAME}/os/${machine};
     pwd;
     ls -l;
-   echo repo-add --new ./sfwtrace-repo.db.tar.tz sfwtrace/os/${machine}/${PACKAGE_EXT}
+    repo-add --new ./sfwtrace.db.tar.gz ${PACKAGE_EXT}
+    ls -l;
 fi;
