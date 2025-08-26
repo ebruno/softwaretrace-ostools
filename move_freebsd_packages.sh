5c26734fa86be76c9566079c154b06e21a39148f
Eric Bruno <eric@ebruno.org>
2025-08-25 20:56:46 -0700

    convert script to tcsh

--- move_freebsd_packages.sh
+++ move_freebsd_packages.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/tcsh
 ARTIFACTDIR="$1";
 PACKAGE_EXT="$2";
 ID="$3";
@@ -7,11 +7,11 @@ REPO_ROOT_PATH="$5";
 REPO_NAME="$6";
 machine="$7";
 
-if [ "${ID}" = "freebsd" ]; then
+if ( "${ID}" == "freebsd" )
     echo find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_PATH}/${ID}/${REPO_NAME}/All\;
     #find "${ARTIFACTDIR}" -name "${PACKAGE_EXT}" -printf "[INFO] Install Package:%f\n" -exec install -p -m 664 {} ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_PATH}/${ID}linux/${REPO_NAME}/All \;
     #cd ${NAS_REPO_ROOT_PATH}/${REPO_ROOT_PATH}/${ID}linux/${REPO_NAME}/os/${machine};
     #chmod a+r *;
     #repo-add -p -R --new ./${REPO_NAME}.db.tar.gz ${PACKAGE_EXT}
     #ls -l;
-fi;
+endif;
